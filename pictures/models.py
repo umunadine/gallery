e8c53406599414e25ed79f612d404ba68d2d3452
umunadine <umutesinad3@gmail.com>
2021-01-05 12:41:03 +0200

    testing location

--- pictures/models.py
+++ pictures/models.py
@@ -7,3 +7,54 @@ class Category(models.Model):
      
     def __str__(self):
         return self.name
+
+    def save_category(self):
+        self.save()
+    
+    
+
+class Location(models.Model):
+    name = models.CharField(max_length =50)
+
+    def __str__(self):
+        return self.name
+
+    @classmethod
+    def tag_articles(cls):
+        tags = cls.objects.all()
+        return tags
+
+    def save_location(self):
+        self.save()
+
+    def delete_location(self):
+        self.delete()
+
+    def update_location(self, update):
+        self.name = update
+        self.save()
+
+    @classmethod
+    def get_location_id(cls, id):
+        locate = Location.objects.get(pk = id)
+        return locate
+
+   
+
+class Image(models.Model):
+    image_name = models.CharField(max_length =30)
+    image_description = models.TextField()
+    image_location = models.ForeignKey(
+        'Location',
+        on_delete=models.CASCADE,
+        )
+    image_category = models.ForeignKey(
+        'Category',
+        on_delete=models.CASCADE,
+        )
+
+    def __str__(self):
+        return self.image_name
+
+    class Meta:
+        ordering = ['image_name']
--- pictures/tests.py
+++ pictures/tests.py
@@ -1,3 +1,23 @@
 from django.test import TestCase
+from .models import Image,Category,Location
 
 # Create your tests here.
+class LocationTestCLass(TestCase):
+    def setUp(self):
+        self.loc = Location(name="Kigali")
+        self.loc.save_location()
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.loc,Location))
+
+    def test_delete_method(self):
+        self.loc.save_location()
+        self.loc.delete_location()
+        location = Location.objects.all()
+        self.assertTrue(len(location) == 0)
+
+    def test_update(self):
+        location = Location.get_location_id(self.loc.id)
+        location.update_location('Paris')
+        location = Location.get_location_id(self.loc.id)
+        self.assertTrue(location.name == 'Paris')
