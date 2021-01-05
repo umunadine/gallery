33c3355dc2f6f75367ffc106f2d4f66af2237ba7
umunadine <umutesinad3@gmail.com>
2021-01-05 11:41:22 +0200

    adding on category model

--- pictures/models.py
+++ pictures/models.py
@@ -4,4 +4,6 @@ import datetime as dt
 # Create your models here.
 class Category(models.Model):
     name = models.CharField(max_length =50)
-
+     
+    def __str__(self):
+        return self.name
