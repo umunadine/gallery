c7c4a7792d48d2308d2304780a27423157e87282
umunadine <umutesinad3@gmail.com>
2021-01-06 03:03:02 +0200

    adding media

--- pictures/models.py
+++ pictures/models.py
@@ -18,14 +18,9 @@ class Category(models.Model):
         self.name = update
         self.save()
 
-    # @classmethod
-    # def get_category_id(cls, id):
-    #     category = Category.objects.get(pk = id)
-    #     return category
-
     @classmethod
-    def search_by_title(cls,search_term):
-        category = cls.objects.filter(name__icontains=search_term)
+    def get_category_id(cls, id):
+        category = Category.objects.get(pk = id)
         return category
     
     
@@ -59,7 +54,7 @@ class Location(models.Model):
    
 
 class Image(models.Model):
-    image = models.ImageField(upload_to = 'pictures/')
+    image = models.ImageField(upload_to = 'pictures/',null=True)
     image_name = models.CharField(max_length =30)
     image_description = models.TextField()
     image_location = models.ForeignKey(
@@ -73,6 +68,31 @@ class Image(models.Model):
 
     def __str__(self):
         return self.image_name
+    
+    def save_image(self):
+        self.save()
+
+    def delete_image(self):
+        self.delete()
+    
+    @classmethod
+    def update_image(cls, id ,name, description , image_location, image_category):
+        update = cls.objects.filter(id = id).update(name = name, description = description ,image_location = image_location,image_category = image_category)
+
+    @classmethod
+    def get_image_by_id(id):
+        image = cls.objects.filter(id=id)
+        return image
+
+    @classmethod
+    def search_image(cls,category):
+        pictures = cls.objects.filter(image_category__name__icontains=category)
+        return pictures
+
+    @classmethod
+    def filter_by_location(cls,location):
+        pictures = cls.objects.filter(image_location=location)
+        return pictures
 
     class Meta:
         ordering = ['image_name']
